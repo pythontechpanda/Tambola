66c5a92d39bcd82267429bca9e36e8a60f109dea
pythondata05 <pythondata05@gmail.com>
2023-11-17 19:30:32 +0530

    Start project

---
+++ tambolaapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TambolaappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tambolaapp'
---
+++ tambolaapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class City(models.Model):
+    city_name=models.CharField(max_length=100)
+    def __str__(self):
+        return self.city_name
+class User(AbstractUser):
+    date_of_birth=models.DateField(null=True)
+    mobile_no=models.CharField(max_length=50,null=True)
+    gender=models.CharField(max_length=50,null=True)
+    city=models.ForeignKey(City,on_delete=models.CASCADE)
+    is_verified=models.BooleanField(default=False)
+    is_above18=models.BooleanField(default=False)
+    
