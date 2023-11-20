becc995147f30bdd0f7fdf95075b869ec4e5cb58
raikwarshubham1997 <raikwarshubham.sr@gmail.com>
2023-11-20 19:07:26 +0530

    admin-panel

--- tambola/urls.py
+++ tambola/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from tambolaapp import views
+from django.conf import settings  
+from django.conf.urls.static import static
+
+router = DefaultRouter()
+# router.register('register', views.UserCreation, basename='register')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('admin-panel/', include("myadmin.urls")),
+    path('api', include("tambolaapp.urls")),
 ]
+
+
+if settings.DEBUG:  
+        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
--- tambolaapp/models.py
+++ tambolaapp/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     date_of_birth=models.DateField(null=True)
     mobile_no=models.CharField(max_length=50,null=True)
     gender=models.CharField(max_length=50,null=True)
-    city=models.ForeignKey(City,on_delete=models.CASCADE)
+    city=models.ForeignKey(City,on_delete=models.CASCADE, null=True)
     is_verified=models.BooleanField(default=False)
     is_above18=models.BooleanField(default=False)
     
